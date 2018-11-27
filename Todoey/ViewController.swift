72782644df61279c550ffaed72862b1cb82f5c69
Taka Suzuki <ooo13@Takas-MacBook-Pro.local>
2018-11-27 08:38:08 -0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Taka Suzuki on 11/27/18.
+//  Copyright © 2018 Taka Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
